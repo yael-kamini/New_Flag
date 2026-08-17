3c8172b3d3ded195b0b14e96615b97d4ccb79920
yael-kamini <yaelkamini10@gmail.com>
2026-08-17 10:14:31 +0300

    Add files via upload

---
+++ flag.py
@@ -0,0 +1,16 @@
+import pygame
+import main
+import Screen
+import consts
+import gamefeild
+import grass
+def create_flag(flag_img):
+    flag = pygame.image.load(flag_img)
+    sized_flag = pygame.transform.scale(flag,(
+        consts.FLAG_WIDTH,consts.FLAG_HEIGHT))
+    flag_box = pygame.Sureface(
+        (consts.FLAG_WIDTH,consts.FLAG_HEIGHT *2),)
+    flag_box.fill(consts.BACKGROUND_COLOR)
+    flag_box.blit(sized_flag,(0,0))
+
+    return flag_box
