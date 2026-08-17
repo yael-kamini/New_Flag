93c57cac4f19c68f04d8c33eb64edbb0c2d40e3e
yael-kamini <yaelkamini10@gmail.com>
2026-08-17 13:55:21 +0300

    Fix mine image loading and blitting in create_mine

--- mine.py
+++ mine.py
@@ -7,12 +7,12 @@ import soldier
 import gamefeild
 
 def create_mine(grass_img):
-    mine = pygame.image.load(MINE_IMG)
+    mine = pygame.image.load(consts.MINE_IMG)
     sized_mine = pygame.transform.scale(mine,(
         consts.MINE_WIDTH,consts.MINE_HEIGHT))
     mine_box = pygame.Sureface(
         (consts.MINE_WIDTH,consts.MINE_HEIGHT *2),)
     mine_box.fill(consts.GREEN)
-    mine_box.blit(sized_grass,(0,0))
+    mine_box.blit(sized_mine,(0,0))
 
     return mine_box
