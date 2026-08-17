45ff56230a42429f199abc4d3e680ebb2a1d9bf6
shellyrazon1 <shelly.razon1@gmail.com>
2026-08-17 13:53:03 +0300

    הפכתי להערה את השורה הראשונ  (זה בMAIN)בSTATE. הוספתי פונק במודול SCREEN וקראתי לה ב MAIN הפכתי את SCREENSIZE לגלובלי והוספתי מודול GRASS ן MINE

---
+++ grass.py
@@ -0,0 +1,21 @@
+import pygame
+import random
+import main
+import Screen
+import consts
+import soldier
+import gamefeild
+import mine
+
+def create_grass(grass_img):
+    grass = pygame.image.load(GRASS_IMG)
+    sized_grass = pygame.transform.scale(grass,(
+        consts.GRASS_WIDTH,consts.GRASS_HEIGHT))
+    grass_box = pygame.Sureface(
+        (consts.GRASS_WIDTH,consts.GRASS_HEIGHT *2),)
+    grass_box.fill(consts.GREEN)
+    grass_box.blit(sized_grass,(0,0))
+
+    return grass_box
+
+
---
+++ mine.py
@@ -0,0 +1,18 @@
+import pygame
+import random
+import main
+import Screen
+import consts
+import soldier
+import gamefeild
+
+def create_mine(grass_img):
+    mine = pygame.image.load(MINE_IMG)
+    sized_mine = pygame.transform.scale(mine,(
+        consts.MINE_WIDTH,consts.MINE_HEIGHT))
+    mine_box = pygame.Sureface(
+        (consts.MINE_WIDTH,consts.MINE_HEIGHT *2),)
+    mine_box.fill(consts.GREEN)
+    mine_box.blit(sized_grass,(0,0))
+
+    return mine_box
